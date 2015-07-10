3aec5923d7ca26209e30a5c90c524caafdb6c2b4
Julienne Arnold <julie.enid@gmail.com>
2015-07-10 00:28:05 -0500

    welcome page logo

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -2,16 +2,34 @@
 @section('style')
 	<style type="text/css">
 		.welcome {
-			padding-top: 150px;
+			padding-top: 150px;			
+			background-image: url('/images/julie-racquet.jpg');
+			background-repeat:  no-repeat;
+			background-position:  center;
+			height: 600px;
+		}
+		.logo {
 		}
 		h1 {
-			font-size: 42pt;
-			font-weight: bolder;
+			font-size: 60pt;
+			font-weight: 900;
 			color: white;
+			text-shadow:
+		    -1px -1px 0 #000,
+		    1px -1px 0 #000,
+		    -1px 4px 0 #000,
+		    1px 1px 0 #000;  
 		}
 		.lead {
+			padding-top: 150px;
 			color: white;
-			font-weight: bolder;
+			font-weight: 800;
+			font-size: 24pt;
+			text-shadow:
+		    -1px -1px 0 #000,
+		    1px -1px 0 #000,
+		    -1px 2px 0 #000,
+		    1px 1px 0 #000;  
 		}
 	</style>
 @stop
@@ -19,10 +37,14 @@
 	<div class="container">
 		<center>
 		<div class="row welcome">
-			<h1>Blah Blah Racquetball</h1>
-			<p class="lead">Play. Prepare. Profound.</p>
-			<br>
-			<a href="{{ url('/auth/register') }}" class="btn btn-default btn-lg">Get Started</a>
+			<h1 class="primary">Racquetball Prep</h1>
+				<br>
+				<br>
+				<a href="{{ url('/auth/register') }}" class="btn btn-primary btn-lg"><b>Get Started</b></a>
+				<br>
+				<p class="lead">Prepare. Perform. Prevail.</p>
+				<br>
+			</div>
 		</div>
 		</center>	
 	</div>
