a26e8237f76da2f851b6d724c704efa1d7d934d9
Julienne Arnold <julie.enid@gmail.com>
2015-09-11 16:08:40 -0500

    Add calls to DatabaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,38 @@ class DatabaseSeeder extends Seeder {
 	{
 		Model::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('DivisionTableSeeder');
+		$this->command->info('Division table seeded!');
+
+		$this->call('EvaluationCategoryTableSeeder');
+		$this->command->info('EvaluationCategory table seeded!');
+
+		$this->call('EvaluationSubCategoryTableSeeder');
+		$this->command->info('EvaluationSubCategory table seeded!');
+
+		$this->call('GameTableSeeder');
+		$this->command->info('Game table seeded!');
+
+		$this->call('GroupTableSeeder');
+		$this->command->info('Group table seeded!');
+		
+		$this->call('LocationTableSeeder');
+        $this->command->info('Location table seeded!');
+
+		$this->call('MatchGameTableSeeder');
+		$this->command->info('MatchGame table seeded!');
+
+		$this->call('SkillTableSeeder');
+		$this->command->info('Skill table seeded!');
+
+		$this->call('TournamentDivisionTableSeeder');
+		$this->command->info('TournamentDivision table seeded!');
+
+		$this->call('UserTableSeeder');
+		$this->command->info('User table seeded!');
+
+		$this->call('VoteTableSeeder');
+		$this->command->info('Vote table seeded!');
 	}
 
 }
