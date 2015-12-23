24a8c5291783bac9adc3683eba33f416eaff2cec
Julienne Arnold <julie.enid@gmail.com>
2015-12-23 11:20:21 -0600

    add shot selections from center/front

--- resources/views/pages/tools/shotselection/index.blade.php
+++ resources/views/pages/tools/shotselection/index.blade.php
@@ -7,6 +7,36 @@
 			height: 600px;
 			width: 310px;
 		}
+		.title {
+			text-align: center
+		}
+		.subtitle{
+			text-align: center
+		}
+		.pass {
+			color:green;
+		}
+		.ceiling {
+			color:orange;
+		}
+		.kill {
+			color:red;
+		}
+		.pinch {
+			color:blue;
+		}	
+		.pass_path{
+			fill:none;stroke:green;stroke-width:2;
+		}
+		.kill_path{
+			fill:none;stroke:red;stroke-width:2;
+		}
+		.ceiling_path{
+			fill:none;stroke:orange;stroke-width:2;
+		}
+		.pinch_path{
+			fill:none;stroke:blue;stroke-width:2;
+		}	
 	</style>
 @stop
 
@@ -14,101 +44,166 @@
 
 <div class="container">
 	<div id="myvue">
-		<div class="row">
+		<div class="row title">
 		<h2>Shot Selection</h2>
 		</div>
 		<div class="row">
-			<div class="col-xs-12">You + Ball + Opponent + Score = Shot Selection</div>
+			<div class="col-xs-12 subtitle"><h3>You + Ball + Opponent + Score = Shot Selection</h3></div>
 		</div>
 		
 		<div class="row">
 			<form class="form-horizontal" role="form">
 				<!-- Dropdowns -->
-				<div class="col-xs-4">
-						<div class="form-group">
-							<label for="off_position" class="control-label">Offense</label>
-							<select id="off_position" v-model="off_position" class="form-control">
-								<option v-for="pos in player.positions" v-bind:value="pos">
-								    @{{ pos }}
-								</option>
-							</select>
-						</div>				
-						<div class="form-group">
-						    <label for="def_position" class="control-label">Defense</label>		
-						    <select id="def_position" v-model="def_position" class="form-control">
-								<option v-for="pos in player.positions" v-bind:value="pos">
-								    @{{ pos }}
-								</option>
-							</select>			    
-						</div>	
-						<div class="form-group">
-						    <label for="ball_speed" class="control-label">Ball Speed</label>	
-						    <select id="ball_speed" v-model="ball_speed" class="form-control">
-								<option v-for="pos in ball.speed" v-bind:value="pos">
-								    @{{ pos }}
-								</option>
-							</select>
-						</div>	
-						<div class="form-group">
-						    <label for="ball_height" class="control-label">Ball Height</label>	
-						    <select id="ball_height" v-model="ball_height" class="form-control">
-								<option v-for="pos in ball.height" v-bind:value="pos">
-								    @{{ pos }}
-								</option>
-							</select>
-						</div>	
-						<div class="form-group">
-						    <label for="ball_angle" class="control-label">Ball Angle</label>	
-						    <select id="ball_angle" v-model="ball_angle" class="form-control">
-								<option v-for="pos in ball.angle" v-bind:value="pos">
-								    @{{ pos }}
-								</option>
-							</select>
-						</div>	
-						<div class="form-group">
-						    <label for="score_range" class="control-label lbl-team indent">Score +/-</label>	
-						    <select id="score_range" v-model="score_range" class="form-control">
-								<option v-for="pos in score.range" v-bind:value="score.range">
-								    @{{ pos }}
-								</option>
-							</select>
-						</div>	
+				<div class="col-xs-3">
+					<div class="form-group">
+						<label for="off_position" class="control-label">Offense</label>
+						<select id="off_position" v-model="off_position" class="form-control" v-on:change="reset_legend()">
+							<option v-for="pos in player.positions" v-bind:value="pos">
+							    @{{ pos }}
+							</option>
+						</select>
+					</div>				
+					<div class="form-group">
+					    <label for="def_position" class="control-label">Defense</label>		
+					    <select id="def_position" v-model="def_position" class="form-control" v-on:change="reset_legend()">
+							<option v-for="pos in player.positions" v-bind:value="pos">
+							    @{{ pos }}
+							</option>
+						</select>			    
+					</div>	
+					<div class="form-group">
+					    <label for="ball_speed" class="control-label">Ball Speed</label>	
+					    <select id="ball_speed" v-model="ball_speed" class="form-control" v-on:change="reset_legend()">
+							<option v-for="pos in ball.speed" v-bind:value="pos">
+							    @{{ pos }}
+							</option>
+						</select>
+					</div>	
+					<div class="form-group">
+					    <label for="ball_height" class="control-label">Ball Height</label>	
+					    <select id="ball_height" v-model="ball_height" class="form-control" v-on:change="reset_legend()">
+							<option v-for="pos in ball.height" v-bind:value="pos">
+							    @{{ pos }}
+							</option>
+						</select>
+					</div>	
+					<div class="form-group">
+					    <label for="ball_angle" class="control-label">Ball Angle</label>	
+					    <select id="ball_angle" v-model="ball_angle" class="form-control" v-on:change="reset_legend()">
+							<option v-for="pos in ball.angle" v-bind:value="pos">
+							    @{{ pos }}
+							</option>
+						</select>
+					</div>	
+					<div class="form-group">
+					    <label for="score_range" class="control-label indent">Score +/-</label>	
+					    <select id="score_range" v-model="score_range" class="form-control" v-on:change="reset_legend()">
+							<option v-for="pos in score.range" v-bind:value="score.range">
+							    @{{ pos }}
+							</option>
+						</select>
+					</div>	
 				</div>
 				<!-- Court Diagram -->
-				<div class="col-xs-8">
+				<div class="col-xs-6">
+					<center>
 					<div class="court-diagram" id="court">						
-						<svg id="court" height="600" width="330">
-
+						<svg id="court" height="600" width="330" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">							
 							<!-- Players Front Court -->
-							<circle v-if="def_position == 'front'" cx="150" cy="200" r="15" stroke="black" stroke-width="2" fill="red" />
-							<circle v-if="off_position == 'front'" cx="160" cy="250" r="15" stroke="black" stroke-width="2" fill="blue" />
+							<circle v-if="show_player(def_position, 'front')" cx="150" cy="200" r="15" stroke="black" stroke-width="2" fill="red" />
+							<circle v-if="show_player(off_position, 'front')" cx="160" cy="250" r="15" stroke="black" stroke-width="2" fill="blue" />
 							<!-- PlayersCenter Court -->
-							<circle v-if="def_position == 'center'" cx="150" cy="350" r="15" stroke="black" stroke-width="2" fill="red" />
-							<circle v-if="off_position == 'center'" cx="160" cy="400" r="15" stroke="black" stroke-width="2" fill="blue" />
+							<circle v-if="show_player(def_position, 'center')" cx="150" cy="350" r="15" stroke="black" stroke-width="2" fill="red" />
+							<circle v-if="show_player(off_position, 'center')" cx="160" cy="400" r="15" stroke="black" stroke-width="2" fill="blue" />
 							<!-- Players Back Court -->
-							<circle v-if="def_position == 'back'" cx="150" cy="500" r="15" stroke="black" stroke-width="2" fill="red" />
-							<circle v-if="off_position == 'back'" cx="160" cy="550" r="15" stroke="black" stroke-width="2" fill="blue" />
+							<circle v-if="show_player(def_position, 'back')" cx="150" cy="500" r="15" stroke="black" stroke-width="2" fill="red" />
+							<circle v-if="show_player(off_position, 'back')" cx="160" cy="550" r="15" stroke="black" stroke-width="2" fill="blue" />
 
+							<!-- Offense = FRONT Defense = FRONT -->
 							<!-- Pass Front Court -->
-							<polyline v-if="off_position == 'front' && def_position == 'front'" points="170,240 200,10 250,500" style="fill:none;stroke:green;stroke-width:2" />
+							<polyline v-if="show_shot('front','front', 1, 1, 1)" points="170,240 200,10 250,500" class="pass_path" />
 							<!-- Ceiling Front Court -->
-							<polyline v-if="off_position == 'front' && def_position == 'front'" points="170,240 200,200, 230, 50, 200,10 230,500" style="fill:none;stroke:orange;stroke-width:2" />
+							<polyline v-if="show_shot('front','front', 3, 2, 20)" points="170,240 200,200, 230, 50, 200,10 230,500" class="ceiling_path" />
 							<!-- Z-shot Front Court -->
-							<polyline v-if="off_position == 'front' && def_position == 'front'" points="170,240 50,10, 10, 50, 300,550 50,550" style="fill:none;stroke:purple;stroke-width:2" />
+							<polyline v-if="show_shot('front','front', 3, 3, 22)" points="170,240 50,10, 10, 50, 300,550 50,550" class="ceiling_path" />
 
+
+							<!-- Offense = FRONT Defense = CENTER -->
+							<polyline v-if="show_shot('front','center', 2, 1, 1)" points="170,240 200,10 205,200" class="kill_path" />
+							<polyline v-if="show_shot('front','center', 4, 30, 2)" points="170,240 200,10 250,500" class="pinch_path" />
+							<polyline v-if="show_shot('front','center', 4, 31, 2)" points="170,240 200,10 250,500" class="pinch_path" />
 							<!-- Pass DTL Center Court -->
-							<polyline v-if="off_position == 'center' && def_position == 'front'" points="180,400 240,10 250,500" style="fill:none;stroke:green;stroke-width:2" />
-							<!-- Pass Cross Center Court -->
-							<polyline v-if="off_position == 'center' && def_position == 'front'" points="180,400 100,10 40,500" style="fill:none;stroke:blue;stroke-width:2" />						
+							<polyline v-if="show_shot('front','center', 1, 1, 3)" points="170,240 200,10 250,500" class="pass_path" />
 							<!-- Pass Wide Center Court -->
-							<polyline v-if="off_position == 'center' && def_position == 'front'" points="180,400 100,10 10,300 250,500" style="fill:none;stroke:red;stroke-width:2" />						<!-- Ceiling Front Court -->
-							<polyline v-if="off_position == 'center' && def_position == 'front'" points="180,400 240,260, 230, 50, 200,10 230,500" style="fill:none;stroke:orange;stroke-width:2" />
-							<!-- Z-shot Front Court -->
-							<polyline v-if="off_position == 'center' && def_position == 'front'" points="180,400 50,10, 10, 50, 300,550 50,550" style="fill:none;stroke:purple;stroke-width:2" />
+							<polyline v-if="show_shot('front','center', 1, 3, 3)" points="180,400 100,10 10,300 250,500" class="pass_path" />
+							
 
+							<!-- Offense = FRONT Defense = BACK -->
+
+
+							<!-- Offense = CENTER Defense = FRONT -->
+							<!-- Pass DTL Center Court -->
+							<polyline v-if="show_shot('center','front', 1, 1, 1)" points="180,400 240,10 250,500" class="pass_path" />
+							<!-- Pass Cross Center Court -->
+							<polyline v-if="show_shot('center','front', 1, 2, 2)" points="180,400 100,10 40,500" class="pass_path" />						
+							<!-- Pass Wide Center Court -->
+							<polyline v-if="show_shot('center','front', 1, 3, 3)" points="180,400 100,10 10,300 250,500" class="pass_path" />						
+							<!-- Ceiling Front Court -->
+							<polyline v-if="show_shot('center','front', 3, 3, 20)" points="180,400 240,260, 230, 50, 200,10 230,500" class="ceiling_path" />
+							<!-- Z-shot Front Court -->
+							<polyline v-if="show_shot('center','front', 3, 3, 22)" points="180,400 50,10, 10, 50, 300,550 50,550" class="ceiling_path" />
+
+
+							<!-- Offense = CENTER Defense = CENTER -->
+
+							<!-- Offense = CENTER Defense = BACK -->
+
+
+
+							<!-- Offense = BACK Defense = FRONT -->
+
+
+
+							<!-- Offense = BACK Defense = CENTER -->
+
+
+
+							<!-- Offense = BACK Defense = BACK -->
 						</svg>						
 					</div>
+					</center>
 				</div>
+				<!-- -->
+				<div class="col-xs-3">
+					<div class="form-group">
+						<label for="shot_types" class="control-label">Shot Types:</label>
+						<select id="shot_types" v-model="show_shot_type" class="form-control" v-on:change="reset_legend()">
+							<option value="All" selected="true">All</option>
+							<option v-for="shot in shot_types" v-bind:value="shot.id">
+							    @{{ shot.type }}
+							</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<label for="shot_types" class="control-label">Shot Preference:</label>
+						<select id="shot_types" v-model="show_shot_pref" class="form-control" v-on:change="reset_legend()">	
+							<option value="All" selected="true">All</option>						
+							<option value="1">1st Only</option>
+							<option value="2">2nd Only</option>
+							<option value="3">3rd Only</option>
+							<option value="4">1st & 2nd Only</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<label for="shot_types" class="control-label">Shots by Name:</label>
+						<div class="well well-sm">
+							<ul v-for="shot in shots" v-if="shot.show">
+								<i class="fa fa-circle" v-bind:class="shot.type"></i> @{{ shot.name }} 
+							</ul>
+						</div>						
+					</div>
+					
+				</div>	
 			</form>
 		</div>
 
@@ -134,7 +229,7 @@
 	<script src="//cdnjs.cloudflare.com/ajax/libs/vue/1.0.1/vue.js"></script>
 	<script>
 
-		Vue.config.debug = false;		
+		Vue.config.debug = true;		
 
 		new Vue({
 			el: '#myvue',
@@ -142,6 +237,8 @@
 				debug: true,
 				off_position: '',
 				def_position: '',
+				show_shot_type: '',
+				show_shot_pref: '',
 				player: {
 						positions: [
 							'front',
@@ -170,9 +267,58 @@
 							'cross'
 						]
 				},
+				shot_types: [ {id:1, type:'pass'}, { id:2, type:'kill'}, {id:3, type:'ceiling'}, {id:4, type:'pinch'}],
+
+				shots: {
+						 1: {type: 'pass', name: 'pass down-the-line', show: false }, 
+						 2: {type: 'pass', name: 'pass cross-court', show: false },
+						 3: {type: 'pass', name: 'pass wide-angle', show: false },
+						 4: {type: 'pass', name: 'over-head pass', show: false },
+						10: {type: 'kill', name: 'kill straight in', show: false }, 
+						11: {type: 'kill', name: 'kill pass', show: false , show: false }, 
+						12: {type: 'kill', name: 'kill pinch', show: false },
+						20: {type: 'ceiling', name: 'ceiling down-the-line', show: false },
+						21: {type: 'ceiling', name: 'ceiling cross-court', show: false }, 
+						22: {type: 'ceiling', name: 'Z-shot', show: false }, 
+						23: {type: 'ceiling', name: 'around the world', show: false },
+						30: {type: 'pinch', name: 'pinch', show: false },
+						31: {type: 'pinch', name: 'reverse pinch', show: false },
+						32: {type: 'pinch', name: 'splat', show: false },
+						33: {type: 'pinch', name: 'fly pinch', show: false },
+						34: {type: 'pinch', name: 'over-head pinch', show: false },
+				},				
 				score: {
 						range: [ -3, 0 +3 ]
 				},
+			},
+			methods: {
+				reset_legend: function(){
+					for (var id in this.shots) {
+						this.shots[id].show = false;
+					};
+				},
+				show_player: function(player_pos, pos){
+					if (player_pos == pos){
+						return true;
+					}else {
+						return false;
+					}					
+				},
+				show_shot: function(off_pos, def_pos, shot_type, shot_pref, shot_id){					
+					//determine if show
+					if ((this.show_shot_type == 'All') || (this.show_shot_type == shot_type)) {
+						if ((this.show_shot_pref == 'All') || (this.show_shot_pref == shot_pref) || ((this.show_shot_pref == 4) && (shot_pref < 3))){
+							if ((this.off_position == off_pos) && (this.def_position == def_pos)){
+								this.shots[shot_id].show = true;							
+								return true;
+							}
+						}
+					}
+					return false;
+				},			
+			},
+			filters: {
+
 			}
 		});		
 	</script>
