e50f84c806bd41ad9d9cf0fc3cc9daa23f248d54
Julienne Arnold <julie.enid@gmail.com>
2015-12-29 12:21:56 -0600

    tool doublesmatchmaker: compare results

--- resources/views/pages/tools/doublesmatcher/index.blade.php
+++ resources/views/pages/tools/doublesmatcher/index.blade.php
@@ -2,7 +2,20 @@
 
 @section('style')
 	<style>
-
+		h3 {
+			text-align: center;
+		}
+		.player {
+			font-size: 12pt;
+			font-weight: 500;
+		}
+		.star {
+			color:green;
+		}
+		.question {
+			font-size: 12pt;
+			font-weight: 500;
+		}
 	</style>
 @stop
 
@@ -10,44 +23,66 @@
 
 <div class="container">
 	<div id="myvue">
-		<div class="panel panel-primary">			
-			<div class="panel-heading"><h3>Questionaire</h3></div>
-			<div class="panel-body">	
-				<div class="row">
-					<div class="col-xs-8">
-						<form class="form-inline" role="form">
-							<ul v-for="q in survey">
-								<h4>@{{q.id}}. @{{ q.question }}</h4>
-								<select v-model="score[q.id]" class="form-control" v-on:change="computeScore">
-								  <option value="0" selected="true"></option>
-								  <option v-for="a in q.answers" v-bind:value="a.weight">
-								    @{{ a.text }}
-								  </option>
-								</select>
-							</ul>
-						</form>
-						<label>@{{total_score}}</label>
+		<div class="row">
+			<div class="col-xs-8">
+				<div class="panel panel-primary">			
+					<div class="panel-heading"><h3>Questionaire</h3></div>
+					<div class="panel-body">	
+						<div class="row">
+							<div class="col-xs-12">
+								<form class="form-inline" role="form">
+									<ul v-for="q in survey">
+										<span class="question">@{{q.id}}. @{{ q.question }}</span>
+										<select v-model="score[q.id]" class="form-control" v-on:change="computeScore">
+										  <option value="0" selected="true"></option>
+										  <option v-for="a in q.answers" v-bind:value="a.weight">
+										    @{{ a.text }}
+										  </option>
+										</select>
+									</ul>
+								</form>
+							</div>
+						</div>
 					</div>
-					<div class="col-xs-4">
-						<ul v-for="p in players | orderBy 'weight'">
-							<h3>@{{p.weight}} @{{p.name}}</h3>
-						</ul>
+				</div>
+			</div>
+			<div class="col-xs-4">
+				<div class="panel panel-success">			
+					<div class="panel-heading"><h3>Matches</h3></div>
+					<div class="panel-body">
+						<div class="row">
+							<div class="col-xs-12">
+								<ul v-for="p in players | orderBy 'match' -1">
+									<div class="row">
+										<div class="col-xs-4">
+											<span class="player">@{{p.name}} </h4>
+										</div>
+										<div class="col-xs-8">
+											<span v-for="s in p.match">
+												<i class ="fa fa-star fa-xs star"></i>
+											</span>
+										</div>
+									</div> 							
+								</ul>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
+	</div>
 
-		<!-- Debug Panel -->
-		<div class="panel panel-default">
-			<div class="panel-body">
-				<div class="row col-xs-12">
-					<input type="checkbox" id="chkDebug" v-model="debug">
-					<label for="chkDebug">Debug</label>
-				</div>
-				<div class="row" v-show = "debug">
-				    @{{ players_list}}
-					<pre>@{{ $data | json }} </pre> 
-				</div>
+
+	<!-- Debug Panel -->
+	<div class="panel panel-default">
+		<div class="panel-body">
+			<div class="row col-xs-12">
+				<input type="checkbox" id="chkDebug" v-model="debug">
+				<label for="chkDebug">Debug</label>
+			</div>
+			<div class="row" v-show = "debug">
+			    @{{ players_list}}
+				<pre>@{{ $data | json }} </pre> 
 			</div>
 		</div>
 	</div>
@@ -73,8 +108,8 @@
 							question: 'Are you a Power player, Control Player, or Balanced Player?', 
 							answers : { 
 								1: { text: 'Power Player',weight: 1 },
-								2: { text: 'Conrol Player', weight: 3 },
-								3: { text: 'Balanced Player', weight: 2 },						
+								3: { text: 'Balanced Player', weight: 0 },									
+								2: { text: 'Control Player', weight: 2 },					
 							}
 						},
 						2: {
@@ -97,8 +132,8 @@
 							id: 4,
 							question: 'I play Calm or Psyched Up?', 
 							answers : { 
-								1: { text: 'Calm', weight: 1 },
-								2: { text: 'Psyched Up', weight: 2 },
+								1: { text: 'Calm', weight: 2 },
+								2: { text: 'Psyched Up', weight: 1 },
 							}
 						},
 						5: {
@@ -120,20 +155,28 @@
 					},
 					players: {
 						1: {id: 1,
-							name: 'Apple',
-							weight: 4
+							name: 'Michael',
+							scores: [1,1,1,1,1,1],
+							diff: [],
+							match: 0,
 						},
 						2: {id: 2,
-							name: 'Banana',
-							weight: 3
+							name: 'Brandon',
+							scores: [0,2,1,1,1,2],
+							diff: [],
+							match: 0,
 						},
 						3: {id: 3,
-							name: 'Orange',
-							weight: 1
+							name: 'Justin',
+							scores: [1,1,1,1,1,2],
+							diff: [],
+							match: 0
 						},
 						4: {id: 4,
-							name: 'Kiwi',
-							weight: 2
+							name: 'Barry',
+							scores: [0,0,1,1,1,1],
+							diff: [],
+							match: 0
 						},					
 				},
 			},								
@@ -143,11 +186,19 @@
 			},				
 			methods: {
 				computeScore: function(){
-				this.total_score = 0;			
+					this.total_score = 0;			
 					for (var id in this.score) {
 						var weight =  parseInt(this.score[id]);
 						this.total_score+=  weight;
-						console.log(this.total_score);
+					};
+
+					for (var pid in this.players) {
+						this.players[pid].match = 0;
+						for (var sid in this.score) {
+							this.players[pid].diff[sid-1] =  Math.abs(this.players[pid].scores[parseInt(sid)-1] - this.score[sid]) ;
+
+							this.players[pid].match += this.players[pid].diff[sid-1]
+						}
 					};
 				}
 			}
